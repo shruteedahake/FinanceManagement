ec80bea8f8ddf00a41be9dabfacf2c96f9a77df0
Shrutee <shrutee242@gmail.com>
2024-05-13 17:42:07 +0530

    Implemented entity classes and User authentication functionality

--- src/util/DBConnUtil.java
+++ src/util/DBConnUtil.java
@@ -1,5 +1,19 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnUtil {
 
+    public static Connection getConnection(String connectionString) {
+        Connection connection = null;
+        try {
+//            connection = DriverManager.getConnection(connectionString);
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fin_mng","root","shrutee@123");            
+        } catch (SQLException e) {
+            System.err.println("Error establishing database connection: " + e.getMessage());
+        }
+        return connection;
+    }
 }
